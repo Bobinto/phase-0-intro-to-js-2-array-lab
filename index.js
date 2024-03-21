cc5c617045014d032e01e5284ba01d71f37dc833
bob <Mansalati18@gmail.com>
2024-03-21 14:42:17 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Write your solution here!
+var cats; 
+
+describe('cats', function () {
+
+    beforeEach(function () {
+        cats = ["Milo", "Otis", "Garfield"]; 
+    });
+
+    it('is assigned an initial value of ["Milo", "Otis", "Garfield"]', function () {
+        expect(cats).toEqual(["Milo", "Otis", "Garfield"]);
+    });
+
+    describe('Array functions', function () {
+        it('destructivelyAppendCat(name)', function () {
+            cats.push("Milo", "Otis", "Garfield"); 
+
+            it('"before each" hook for "appends a cat to the end of the cats array"', function () {
+                
+            });
+        });
+    });
+});
